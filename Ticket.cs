5262be29b5b89440ffb6582cf152c0e98a3dbb43
zhendrix <zhendrix@my.wctc.edu>
2023-10-09 18:24:47 -0500

    **Amended ticket class and made it correctly

---
+++ Ticket.cs
@@ -0,0 +1,16 @@
+class Ticket
+{
+    public int TicketID { get; set; }
+    public string Summary { get; set; }
+    public string Status { get; set; }
+    public string Priority { get; set; }
+    public string Submitter { get; set; }
+    public string Assigned { get; set; }
+    public List<string> Watching { get; set; }
+
+    public string ToCsvString()
+    {
+        string watching = string.Join("|", Watching);
+        return $"{TicketID},{Summary},{Status},{Priority},{Submitter},{Assigned},{watching}";
+    }
+}
