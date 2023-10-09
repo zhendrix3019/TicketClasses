7b9b02dd1b9ddcc12b63f804bd0d479d8fe87ae3
zhendrix <zhendrix@my.wctc.edu>
2023-10-09 18:24:01 -0500

    Created Main program.cs for classes to run from

--- Program.cs
+++ Program.cs
@@ -1,14 +1,52 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-
-class Ticket
+﻿class Program
 {
-    public int TicketID { get; set; }
-    public string Summary { get; set; }
-    public string Status { get; set; }
-    public string Priority { get; set; }
-    public string Submitter { get; set; }
-    public string Assigned { get; set; }
-    public List<string> Watching { get; set; }
+    static void Main(string[] args)
+    {
+        string csvFilePath = "Tickets.csv";
+        TicketManager ticketManager = new TicketManager(csvFilePath);
+
+        while (true)
+        {
+            Console.WriteLine("Enter Ticket ID:");
+            int ticketId = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter Summary:");
+            string summary = Console.ReadLine();
+
+            Console.WriteLine("Enter Status:");
+            string status = Console.ReadLine();
+
+            Console.WriteLine("Enter Priority:");
+            string priority = Console.ReadLine();
+
+            Console.WriteLine("Enter Submitter:");
+            string submitter = Console.ReadLine();
+
+            Console.WriteLine("Enter Assigned:");
+            string assigned = Console.ReadLine();
+
+            Console.WriteLine("Enter Watching (separated by '|'):");
+            string watchingInput = Console.ReadLine();
+            List<string> watching = new List<string>(watchingInput.Split('|'));
+
+            Ticket newTicket = new Ticket
+            {
+                TicketID = ticketId,
+                Summary = summary,
+                Status = status,
+                Priority = priority,
+                Submitter = submitter,
+                Assigned = assigned,
+                Watching = watching
+            };
+
+            ticketManager.AddTicket(newTicket);
+            Console.WriteLine("Record added successfully!");
+
+            Console.WriteLine("Do you want to add another record? (yes/no)");
+            string anotherRecord = Console.ReadLine().ToLower();
+            if (anotherRecord != "yes")
+                break;
+        }
+    }
 }
