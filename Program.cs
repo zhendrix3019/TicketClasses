7c25f334b8d13f9b1031de18f11f27abbdef1979
zhendrix <zhendrix@my.wctc.edu>
2023-10-09 18:18:14 -0500

    Created Ticket class

--- Program.cs
+++ Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+class Ticket
+{
+    public int TicketID { get; set; }
+    public string Summary { get; set; }
+    public string Status { get; set; }
+    public string Priority { get; set; }
+    public string Submitter { get; set; }
+    public string Assigned { get; set; }
+    public List<string> Watching { get; set; }
+}
