2286d12e420152a20965c9b0ce9671cc53bac7c4
zhendrix <zhendrix@my.wctc.edu>
2023-10-23 16:57:43 -0500

    Added bugticket class

---
+++ BugTicket.cs
@@ -0,0 +1,13 @@
+﻿namespace TicketApp.Models
+{
+    class BugTicket : BaseTicket
+    {
+        public int Severity { get; set; }
+
+        public string ToCsvString()
+        {
+            string watching = string.Join("|", Watching);
+            return $"{TicketID},{Summary},{Status},{Priority},{Submitter},{Assigned},{watching},{Severity}";
+        }
+    }
+}
