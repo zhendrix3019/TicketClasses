eb8e1b06dcbcf4b77267e2615407918cc97ed1ec
zhendrix <zhendrix@my.wctc.edu>
2023-10-23 16:58:23 -0500

    Added the EnhancementTicket class

---
+++ EnhancementTicket.cs
@@ -0,0 +1,16 @@
+﻿namespace TicketApp.Models
+{
+    class EnhancementTicket : BaseTicket
+    {
+        public string Software { get; set; }
+        public string Cost { get; set; }
+        public string Reason { get; set; }
+        public string Estimate { get; set; }
+
+        public string ToCsvString()
+        {
+            string watching = string.Join("|", Watching);
+            return $"{TicketID},{Summary},{Status},{Priority},{Submitter},{Assigned},{watching},{Software},{Cost},{Reason},{Estimate}";
+        }
+    }
+}
